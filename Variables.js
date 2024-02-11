c948a38d93e52b4e66197f7f4daa2d56ae3763f8
Anjali <anjalinegian2002@gmail.com>
2024-02-12 01:03:02 +0530

    Variable in JS

---
+++ Variables.js
@@ -0,0 +1,28 @@
+// intro to variables
+
+// variables can store some inforamtion
+// we can use that information later
+// we can change that information later
+
+// declare a variable (using val and let)
+var firstName = "Anjali";
+let lastName  = "negi";
+
+// declare a const (using const)
+const completeName = 'Anjali negi'
+// use a variable
+console.log(firstName);
+console.log(lastName);
+console.log(completeName)
+
+// change value
+firstName = 'Rita';
+
+
+// rules for naming variables
+// 1. can't start with number (wrong 1value)
+// 2. can use only undersore ( _ ) or dollar($) symbol 
+// 3. can't use spaces first name (invalid).
+
+// convention
+// start with small letter and user camelCase
